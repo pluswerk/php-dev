5094b8bbf3ad1747d17ff2ffc6ae1c7cac7a3bc4
Matthias Vogel <m.vogel@andersundsehr.com>
2018-12-13 15:33:15 +0100

    :whale::heavy_minus_sign: simplify docker usage for node container

--- .additional_bashrc.sh
+++ .additional_bashrc.sh
@@ -5,11 +5,11 @@ source ~/.git-prompt.sh
 
 PS1='\033]2;'$(pwd)'\007\[\e[0;36m\][\[\e[1;31m\]\u\[\e[0;36m\]@\[\e[1;34m\]\h\[\e[0;36m\]: \[\e[0m\]\w\[\e[0;36m\]]\[\e[0m\]\$\[\e[1;32m\]\s\[\e[0;33m\]$(__git_ps1)\[\e[0;36m\]> \[\e[0m\]\n$ ';
 
-DOCKER_COMPOSE_PROJECT=$(docker inspect ${HOSTNAME} | grep '"com.docker.compose.project":' | awk '{print $2}' | tr --delete '"' | tr --delete ',')
-NODE_CONTAINER=$(docker ps -f "name=${DOCKER_COMPOSE_PROJECT}_node_1" --format {{.Names}})
-alias node='docker exec -w $(pwd) -it ${NODE_CONTAINER} node'
-alias npm='docker exec -w $(pwd) -it ${NODE_CONTAINER} npm'
-alias yarn='docker exec -w $(pwd) -it ${NODE_CONTAINER} yarn'
+DOCKER_COMPOSE_PROJECT=$(sudo docker inspect ${HOSTNAME} | grep '"com.docker.compose.project":' | awk '{print $2}' | tr --delete '"' | tr --delete ',')
+NODE_CONTAINER=$(sudo docker ps -f "name=${DOCKER_COMPOSE_PROJECT}_node_1" --format {{.Names}})
+alias node='sudo docker exec -w $(pwd) -it ${NODE_CONTAINER} node'
+alias npm='sudo docker exec -w $(pwd) -it ${NODE_CONTAINER} npm'
+alias yarn='sudo docker exec -w $(pwd) -it ${NODE_CONTAINER} yarn'
 
 # Run SSH Agent and add key 7d
 if [ -z "$SSH_AUTH_SOCK" ] ; then
