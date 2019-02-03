ca549ac13b29ce34fdaa14c7358d20036cc30f45
Cyb10101 <cyb10101@gmail.com>
2019-02-03 12:10:36 +0100

    [TASK] Set user and group id for node container

--- .additional_bashrc.sh
+++ .additional_bashrc.sh
@@ -7,9 +7,9 @@ PS1='\033]2;'$(pwd)'\007\[\e[0;36m\][\[\e[1;31m\]\u\[\e[0;36m\]@\[\e[1;34m\]\h\[
 
 DOCKER_COMPOSE_PROJECT=$(sudo docker inspect ${HOSTNAME} | grep '"com.docker.compose.project":' | awk '{print $2}' | tr --delete '"' | tr --delete ',')
 NODE_CONTAINER=$(sudo docker ps -f "name=${DOCKER_COMPOSE_PROJECT}_node_1" --format {{.Names}})
-alias node='sudo docker exec -u1000 -w $(pwd) -it ${NODE_CONTAINER} node'
-alias npm='sudo docker exec -u1000 -w $(pwd) -it ${NODE_CONTAINER} npm'
-alias yarn='sudo docker exec -u1000 -w $(pwd) -it ${NODE_CONTAINER} yarn'
+alias node='sudo docker exec -u $(id -u):$(id -g) -w $(pwd) -it ${NODE_CONTAINER} node'
+alias npm='sudo docker exec -u $(id -u):$(id -g) -w $(pwd) -it ${NODE_CONTAINER} npm'
+alias yarn='sudo docker exec -u $(id -u):$(id -g) -w $(pwd) -it ${NODE_CONTAINER} yarn'
 
 # Run SSH Agent and add key 7d
 if [ -z "$SSH_AUTH_SOCK" ] ; then
