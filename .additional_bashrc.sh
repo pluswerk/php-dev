bf6558f9ae737a361a6f63e12b464291cdfa73ed
Chris Ben <chris.ben9@gmail.com>
2019-02-11 11:27:52 +0100

    adding basic exec commands for node as alias

--- .additional_bashrc.sh
+++ .additional_bashrc.sh
@@ -10,6 +10,8 @@ NODE_CONTAINER=$(sudo docker ps -f "name=${DOCKER_COMPOSE_PROJECT}_node_1" --for
 alias node='sudo docker exec -u1000 -w $(pwd) -it ${NODE_CONTAINER} node'
 alias npm='sudo docker exec -u1000 -w $(pwd) -it ${NODE_CONTAINER} npm'
 alias yarn='sudo docker exec -u1000 -w $(pwd) -it ${NODE_CONTAINER} yarn'
+alias node_exec='sudo docker exec -u1000 -w $(pwd) -it ${NODE_CONTAINER}'
+alias node_root_exec='sudo docker exec -w $(pwd) -it ${NODE_CONTAINER}'
 
 # Run SSH Agent and add key 7d
 if [ -z "$SSH_AUTH_SOCK" ] ; then
