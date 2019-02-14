408ac6f836ede6e60d12094dc32e11db4f6ebb25
Matthias Vogel <git@kanti.de>
2019-02-14 18:03:19 +0100

    [TASK] Application group added in nginx user configuration
    Co-Authored-By: Cyb10101 <cyb10101@gmail.com>

--- entrypoint.d/webserver.sh
+++ entrypoint.d/webserver.sh
@@ -6,7 +6,7 @@ if [ -f /etc/apache2/envvars ]; then
    sed -i "s/^\(export APACHE_RUN_GROUP\)=.*/\1=${APPLICATION_GROUP}/" /etc/apache2/envvars;
 fi
 
-# Set nginx user
+# Set nginx user & group
 if [ -f /etc/nginx/nginx.conf ]; then
    sed -i "s/^user www-data;/user ${APPLICATION_USER} ${APPLICATION_GROUP};/" /etc/nginx/nginx.conf
 fi
