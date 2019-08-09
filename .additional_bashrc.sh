d3ef4d3fa15bed64631d7975c98cdb0695623b76
Cyb10101 <cyb10101@gmail.com>
2019-08-09 13:39:06 +0200

    Test if docker socket exist

--- .additional_bashrc.sh
+++ .additional_bashrc.sh
@@ -6,7 +6,7 @@ source ~/.git-prompt.sh
 # only enable docker aliases if docker-socket is mounted
 CONTAINER_ID=$(basename $(cat /proc/1/cpuset))
 export HOST_DISPLAY_NAME=$HOSTNAME
-if sudo docker ps -q &>/dev/null; then
+if test -S "/var/run/docker.sock"; then
   DOCKER_COMPOSE_PROJECT=$(sudo docker inspect ${CONTAINER_ID} | grep '"com.docker.compose.project":' | awk '{print $2}' | tr --delete '"' | tr --delete ',')
   export NODE_CONTAINER=$(sudo docker ps -f "name=${DOCKER_COMPOSE_PROJECT}_node_1" --format {{.Names}})
   export HOST_DISPLAY_NAME=$(sudo docker inspect ${CONTAINER_ID} --format='{{.Name}}')
