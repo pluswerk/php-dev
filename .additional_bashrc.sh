dc997cdc362a8330eb2fed3c286a10867028fed3
Cyb10101 <cyb10101@gmail.com>
2019-08-28 17:21:22 +0200

    Remove node aliases and add as shell file

--- .additional_bashrc.sh
+++ .additional_bashrc.sh
@@ -8,17 +8,8 @@ CONTAINER_ID=$(basename $(cat /proc/1/cpuset))
 export HOST_DISPLAY_NAME=$HOSTNAME
 if sudo docker ps -q &>/dev/null; then
   DOCKER_COMPOSE_PROJECT=$(sudo docker inspect ${CONTAINER_ID} | grep '"com.docker.compose.project":' | awk '{print $2}' | tr --delete '"' | tr --delete ',')
-  export NODE_CONTAINER=$(sudo docker ps -f "name=${DOCKER_COMPOSE_PROJECT}_node_1" --format {{.Names}})
   export HOST_DISPLAY_NAME=$(sudo docker inspect ${CONTAINER_ID} --format='{{.Name}}')
   export HOST_DISPLAY_NAME=${HOST_DISPLAY_NAME:1}
-
-  alias node_exec='sudo docker exec -u $(id -u):$(id -g) -w $(pwd) -it ${NODE_CONTAINER}'
-  alias node_root_exec='sudo docker exec -w $(pwd) -it ${NODE_CONTAINER}'
-
-  alias node='node_exec node'
-  alias npm='node_exec npm'
-  alias npx='node_exec npx'
-  alias yarn='node_exec yarn'
 fi;
 
 if [[ $CONTAINER_ID != ${HOSTNAME}* ]] ; then
