1c16d1fdca268b7d6011899733b2a67ea3b3f699
Matthias Vogel <m.vogel@andersundsehr.com>
2020-02-24 17:02:06 +0100

    :white_check_mark: update tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,18 @@
 
 # php should be callable
 php -v &&
-# php should have specific modules installed
-xdebug-enable && php -m | grep xdebug || exit 1 &&
-xdebug-disable && php -m | grep -v xdebug || exit 1 &&
-[ 70000 -le $(php -r "echo PHP_VERSION_ID;") ] || php -m | grep tideways || exit 1 &&
+xdebug-enable &&
+# test xdebug is enabled:
+test "$(php -m | grep xdebug)" = "xdebug" &&
+# test xhprof is disabled:
+test "$(php -i | grep auto_prepend_file)" = "auto_prepend_file => no value => no value" &&
+xdebug-disable &&
+# test xdebug is disabled:
+test "$(php -m | grep xdebug)" = "" &&
+# test xhprof is enabled:
+test "$(php -i | grep auto_prepend_file)" = "auto_prepend_file => /opt/docker/profiler.php => /opt/docker/profiler.php" &&
+# test tideways is installed
+test "$(php -m | grep tideways)" = "tideways_xhprof" &&
 # sudo should be installed
 sudo echo 'done' &&
 # nano should be installed
@@ -28,7 +36,7 @@ docker --version &&
 composer --version &&
 # timezone should be berlin
 cat /etc/timezone &&
-test $(cat /etc/timezone) = "Europe/Berlin" || exit 1 &&
-test $(php -r 'echo date_default_timezone_get();') = "Europe/Berlin" || exit 1 &&
+test $(cat /etc/timezone) = "Europe/Berlin" &&
+test $(php -r 'echo date_default_timezone_get();') = "Europe/Berlin" &&
 
 exit $?
