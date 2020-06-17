abf1f0b726dfabac7f0c252f1ee37a9a2004145e
Matthias Vogel <git@kanti.de>
2020-06-17 16:56:53 +0200

    :pencil: update wording

--- start.sh
+++ start.sh
@@ -42,7 +42,7 @@ function startFunction {
         docker-compose --project-directory . -f compose/${CONTEXT}/docker-compose.yml exec -u $USER node bash
         return
         ;;
-     # this is use full if you ssh into your server and than want to execute something in your php container with ssh access (eg. composer install)
+     # this is useful if you need to have your ssh-agent available for executing a command e.g composer install
      # example usage: bash start.sh run:ssh -T web composer install --no-dev --ansi --verbose
      run:ssh)
         docker-compose --project-directory . -f compose/${CONTEXT}/docker-compose.yml run -u $USER --rm --entrypoint= $SSH_AGENT_PART ${@:2}
