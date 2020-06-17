7706353dc9fcf8366ac661969302eddd5be9eca6
Matthias Vogel <git@kanti.de>
2020-06-17 16:12:28 +0200

    add ssh auth socket into running container
    resolves: #102

--- start.sh
+++ start.sh
@@ -9,6 +9,13 @@ CONTEXT=${CONTEXT:-Development}
 
 USER=${APPLICATION_UID:-1000}:${APPLICATION_GID:-1000}
 
+if [ -z "$SSH_AUTH_SOCK" ]
+then
+  SSH_AGENT_PART=
+else
+  SSH_AGENT_PART="-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
+fi
+
 function startFunction {
   key="$1"
   case ${key} in
@@ -33,6 +40,12 @@ function startFunction {
         ;;
      login:node)
         docker-compose --project-directory . -f compose/${CONTEXT}/docker-compose.yml exec -u $USER node bash
+        return
+        ;;
+     # this is use full if you ssh into your server and than want to execute something in your php container with ssh access (eg. composer install)
+     # example usage: bash start.sh run:ssh -T web composer install --no-dev --ansi --verbose
+     run:ssh)
+        docker-compose --project-directory . -f compose/${CONTEXT}/docker-compose.yml run -u $USER --rm --entrypoint= $SSH_AGENT_PART ${@:2}
         return
         ;;
      *)
