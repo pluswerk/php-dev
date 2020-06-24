2764e173dd3ae15afaf7185da9c52f327f9480ef
Matthias Vogel <git@kanti.de>
2020-06-24 11:26:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -44,7 +44,7 @@ function startFunction {
         ;;
      # this is useful if you need to have your ssh-agent available for executing a command e.g composer install
      # example usage: bash start.sh run:ssh -T web composer install --no-dev --ansi --verbose
-     run:ssh)
+     run-with-agent)
         docker-compose --project-directory . -f compose/${CONTEXT}/docker-compose.yml run -u $USER --rm --entrypoint= $SSH_AGENT_PART ${@:2}
         return
         ;;
