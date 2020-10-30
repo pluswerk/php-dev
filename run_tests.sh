5623aaff1d049e9b49f02b79a92112380c081275
Matthias Vogel <m.vogel@andersundsehr.com>
2020-10-30 14:37:13 +0100

    🐛 fix run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -14,8 +14,10 @@ test "$(php -m | grep xdebug)" = "" &&
 test "$(php -i | grep auto_prepend_file)" = "auto_prepend_file => /opt/docker/profiler.php => /opt/docker/profiler.php" &&
 # test tideways is installed
 test "$(php -m | grep tideways)" = "tideways_xhprof" &&
-# is hirak/prestissimo installed in composer
-[[ "$(composer global show 2>&1 | grep 'hirak/prestissimo')" =~ ^hirak/prestissimo.* ]] &&
+# is hirak/prestissimo installed in composer1
+[[ "$(composer1 global show 2>&1 | grep 'hirak/prestissimo')" =~ ^hirak/prestissimo.* ]] &&
+# is hirak/prestissimo installed in composer1
+[[ "$(composer2 global show 2>&1 | grep 'davidrjonas/composer-lock-diff')" =~ ^davidrjonas/composer-lock-diff.* ]] &&
 # sudo should be installed
 sudo echo 'done' &&
 # nano should be installed
@@ -35,7 +37,9 @@ ping -V &&
 # docker should be installed
 docker --version &&
 # composer should be installed
-composer --version &&
+composer1 --version &&
+# composer should be installed
+composer2 --version &&
 # timezone should be berlin
 cat /etc/timezone &&
 test $(cat /etc/timezone) = "Europe/Berlin" &&
